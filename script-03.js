5f5eec9a82813e10aa746e16ab6a15d0d51f6f88
Ana Luiza <ana.lpeixoto@a.ucb.br>
2025-06-07 12:18:31 -0300

    07/06/2025

--- script-03.js
+++ script-03.js
@@ -1,10 +1,16 @@
-function parImpar(){
+function maiorValor(){
   var a = parseInt(document.getElementById('valor01').value);
+  var b = parseInt(document.getElementById('valor02').value);
+  var c = parseInt(document.getElementById('valor03').value);
  
-  if(a % 2 == 0){
-    document.getElementById('demo').innerHTML = "É um número par.";
+  var maior =matchMedia(a, b, c)
+
+  if(a>b && a>b){
+    document.getElementById('demo').innerHTML = a;
+  }else if(b>a && b>c){
+    document.getElementById('demo').innerHTML = b;
   }else{
-    document.getElementById('demo').innerHTML = "É um número ímpar.";
+    document.getElementById('demo').innerHTML = c;
   }
 
 }
